9aba4cb4c8238415169b60bfd84a9d907aaf8407
SamarthD1 <samarthd470@gmail.com>
2025-01-15 21:22:20 +0530

    Add files via upload

---
+++ 59.Command-LineArguments.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+logging.info("This is an informational message.")
+logging.error("This is an error message.")
+logging.warning("This is a warning message.")
+
+print("Logs have been written to app.log.")
